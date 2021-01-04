b79c4b67caf508948532516df5b98f9b877249f2
BomL <0955163003@ukr.net>
2021-01-05 00:37:57 +0300

    Разработка инженерки всё идёт и идёт по плану

---
+++ SSM13/Assets/Scripts/Power.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Power : MonoBehaviour
+{
+    public int ConsumeEnergy = 4000;
+    private int ProduceEnergy;
+  
+    public int Capacity = 2000000; 
+    private int chargeLevel = 102;
+
+    //  public int chargePercent = 100 * chargeLevel / Capacity;
+
+
+     public float ChargePercent { get => Mathf.Round(100 * chargeLevel / Capacity); }
+   
+    
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+     void FixedUpdate()
+    {
+        
+    }
+}
