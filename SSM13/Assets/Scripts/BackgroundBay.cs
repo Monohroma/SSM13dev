9267edebaf584ba2852f93d42b8cb444c67b60f7
BomL <0955163003@ukr.net>
2021-01-12 01:22:19 +0300

    $Сделал возможность покупки {name}
    TextMesh -  в подарок. (Возможность покупки ещё не сделал, только добавил затемнение с текстом поверх, сейчас не хочу возиться с синглтоном иккувана)

---
+++ SSM13/Assets/Scripts/BackgroundBay.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+//    //текст при наведении //    //текст если не наведено 
+public class BackgroundBay : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public GameObject yy_text;
+    public void OnPointerEnter(PointerEventData eventData) 
+    {
+        yy_text.SetActive(true);
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        yy_text.SetActive(false);
+    }
+}
+
