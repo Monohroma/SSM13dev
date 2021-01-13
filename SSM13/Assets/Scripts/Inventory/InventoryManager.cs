cfbc5077af8feaf4b29daa7ebb2d90fc3bd855bf
Mikhail <sovremen1@gmail.com>
2021-01-13 22:30:40 +0300

    Inventory init

---
+++ SSM13/Assets/Scripts/Inventory/InventoryManager.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+	private List<Item> items;
+}
---
+++ SSM13/Assets/Scripts/Inventory/Item.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+class Item : MonoBehaviour
+{
+	private ITEMTYPE type = ITEMTYPE.FOOD;
+	private ITEMNAME name = ITEMNAME.POTATO;
+	private uint count = 0;
+}
+
+public enum ITEMNAME
+{
+	POTATO,
+	HOTPOTATO
+}
+
+public enum ITEMTYPE
+{
+	FOOD,
+	GUN
+}
