65fe37db4a5a32c59228e09c45b672347538913f
SnDandi <56317121+SnDandi@users.noreply.github.com>
2021-01-16 17:43:10 +0300

    Revert "Revert "доработочка хидропоники""
    This reverts commit edbcedd2879af362edb87869deed5bf6ea46dd2a.

--- SSM13/Assets/Scripts/Bay.cs
+++ SSM13/Assets/Scripts/Bay.cs
@@ -11,11 +11,13 @@ namespace Bay
         public string Name { get; private set; }
         public int Consumption { get; set; }
         public bool Bought { get; set; }
-        public Bay(string Name, bool Bought, int Consumption)
+        public int cost { get; set; }
+        public Bay(string Name, bool Bought, int Consumption, int cost)
         {
             this.Name = Name;
             this.Bought = Bought;
             this.Consumption = Consumption;
+            this.cost = cost;
         }
 
     }
