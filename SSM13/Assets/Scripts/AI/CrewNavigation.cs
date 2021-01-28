9863e92100e3f777b9b79358587319792b6b2f8c
Mihail <48523152+SexyTolik@users.noreply.github.com>
2021-01-29 03:58:02 +0500

    продолжаем разбиратся с A*
    Закодил поиск пути для нсп, но без передвижения и со статичной конечной точкой пути

---
+++ SSM13/Assets/Scripts/AI/CrewNavigation.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class CrewNavigation : MonoBehaviour
+{                                           // ПОИСК ПУТИ ЕСТЬ, А ХОДЬБЫ ПО ТАЙЛАМ НЕТУ! докожу позже
+    public Transform target;
+
+    public float speed = 200f;
+    public float nextWaypointDistanse = .32f;
+
+    Path path;
+    int currentWaypoint = 0;
+    bool reacheEndOfPath = false;
+
+    Seeker seeker;
+    void Start()
+    {
+        seeker = GetComponent<Seeker>();
+
+        InvokeRepeating("UpdatePath", 0f, .5f);
+
+    }
+
+    void UpdatePath()
+    {
+        if (seeker.IsDone())
+        seeker.StartPath(transform.position, target.position, OnPathComplete);
+    }
+    void OnPathComplete(Path p)
+    {
+        if(!p.error)
+        {
+            path = p;
+            currentWaypoint = 0;
+        }
+    }
+    void FixedUpdate()
+    {
+        if (path == null)
+            return;
+
+        if(currentWaypoint >= path.vectorPath.Count)
+        {
+            reacheEndOfPath = true;
+            return;
+        }
+        else
+        {
+            reacheEndOfPath = false;
+        }
+
+    }
+}
