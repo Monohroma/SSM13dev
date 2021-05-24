0128ac86eaa1518a19e6413fc4642357e8ff2f58
Monohroma <84721301+Monohroma@users.noreply.github.com>
2021-05-24 20:45:04 +0400

    Переработка энергии
    Добавлен класс генератор. Теперь солнечная панель наследуется от него. Класс Power был костыльно переделан для показа значения энергии от Energetics. Немного переделан Energetucs.

---
+++ SSM13/Assets/Scripts/Ark/Generator.cs
@@ -0,0 +1,39 @@
+using Ark;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generator : MonoBehaviour
+{
+    public int power { get { return _power; } set { SetPower(value); } }
+    public bool working { get { return _working; } set { SetWorking(value); } }
+
+    [SerializeField]protected bool _working = false;
+    [SerializeField] protected int _power = 100;
+
+    private void Start()
+    {
+        Energetics.AddGenerator(this);
+    }
+
+    private void OnDestroy()
+    {
+        Energetics.RemoveGenerator(this);
+    }
+
+    public virtual void Generate()
+    {
+        if(_working)
+            Energetics.Instance.AddEnergy(_power);
+    }
+
+    public virtual void SetWorking(bool work)
+    {
+        _working = work;
+    }
+
+    public virtual void SetPower(int power)
+    {
+        _power = power;
+    }
+}
--- SSM13/Assets/Scripts/Station/Bays/engineering/Solar.cs
+++ SSM13/Assets/Scripts/Station/Bays/engineering/Solar.cs
@@ -1,22 +1,25 @@
-﻿using System.Collections;
+﻿using Ark;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Solar : MonoBehaviour
+public class Solar : Generator
 {
-    public bool Bought = false;
+    public float dirt = 0;
+    public float dirting_speed = 0.01f;
 
-    void Update()
+    private void FixedUpdate()
     {
-        GetComponent<SpriteRenderer>().enabled = Bought; 
-        if (Bought)
-        {
-            gameObject.name = "bought";
-        }
-        else 
-        {
-            gameObject.name = "unbought";
-        }
+        if (dirt < 1)
+            dirt += Time.fixedDeltaTime * dirting_speed;
+        else
+            dirt = 1;
+    }
+
+    public override void Generate()
+    {
+        if (_working)
+            Energetics.Instance.AddEnergy(Mathf.RoundToInt(_power * (1 - dirt)));
     }
 }
