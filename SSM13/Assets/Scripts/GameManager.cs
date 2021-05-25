b3bff66511d57bac2d520721c870feb92d9309b1
Monohroma <84721301+Monohroma@users.noreply.github.com>
2021-05-25 12:58:50 +0400

    Переработка энергии 2
    Изменения в отсеках, теперь они добавляют себя в list GameManager, и отнимают энергию в Energetics. Генераторы теперь добавляет себя не в Energetics, а в GameManager.

--- SSM13/Assets/Scripts/GameManager.cs
+++ SSM13/Assets/Scripts/GameManager.cs
@@ -1,25 +1,46 @@
 ﻿using System.Collections.Generic;
 using System.Timers;
 using UnityEngine;
+using Ark;
 
 public class GameManager : MonoBehaviour
 {
-    private static GameManager instance;
+    private static GameManager _instance;
+	public List<Generator> currentGenerators = new List<Generator>();
+	public List<Bay> currentBays = new List<Bay>();
 
-    private GameManager()
-	{
-		
-	}
+    private void Awake()
+    {
+		_instance = this;
+    }
 
-	public static GameManager Instance
+    public static GameManager Instance
 	{
 		get
 		{
-			if (instance == null)
-			{
-				instance = new GameManager();
-			}
-			return instance;
+			return _instance;
 		}
 	}
+
+	public void AddGenerator(Generator generator)
+    {
+		if(!currentGenerators.Contains(generator))
+			currentGenerators.Add(generator);
+    }
+
+	public void RemoveGenerator(Generator generator)
+	{
+		currentGenerators.Remove(generator);
+	}
+
+	public void AddBay(Bay bay)
+    {
+		if (!currentBays.Contains(bay))
+			currentBays.Add(bay);
+    }
+
+	public void RemoveBay(Bay bay)
+    {
+		currentBays.Remove(bay);
+    }
 }
