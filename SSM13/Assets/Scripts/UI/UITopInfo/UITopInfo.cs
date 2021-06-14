9ce919565c1bf6d93ae49dc77860d04106d4d501
Mikhail <sovremen1@gmail.com>
2021-06-14 10:15:01 +0300

    Оптимизация (наверное) показателей ресурсов

--- SSM13/Assets/Scripts/UI/UITopInfo/UITopInfo.cs
+++ SSM13/Assets/Scripts/UI/UITopInfo/UITopInfo.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,14 +11,44 @@ public class UITopInfo : MonoBehaviour
     public TMP_Text energyText;
     public TMP_Text humansText;
 
-    private void Update()
+
+	private void Awake()
+	{
+        Energetics.Instance.EnergyChanged += UpdateEnergy;
+        Economics.Instance.MoneyChanged += UpdateMoney;
+    }
+	private void Start()
+    {
+        UpdateEnergy(Energetics.Instance.StoredEnergy);
+        UpdateMoney(Economics.Instance.StoredMoney);
+    }
+
+    private void UpdateEnergy(int power)
     {
         if (Energetics.Instance.StoredEnergy >= Energetics.Instance.MaxEnergy)
+        {
             energyText.color = Color.red;
+        }
         else
+        {
             energyText.color = Color.white;
-        energyText.text = Energetics.Instance.StoredEnergy.ToString();
-        moneyText.text = Economics.Instance.StoredMoney.ToString();
-        humansText.text = GameManager.Instance.FreeAssistant.Count + $" /{GameManager.Instance.AllCrew.Count}";
+        }
+        energyText.text = power.ToString();
+    }
+
+    private void UpdateMoney(int money)
+	{
+        moneyText.text = money.ToString();
+    }
+
+    private void UpdateCrew(int count)
+	{
+        throw new NotImplementedException();
+	}
+    
+    private void Update()
+    {
+        // ¬сЄ что св€зано с персоналом реализовано через дикий говнокод, так что не стал это убирать, чтобы ничего не сломалось.
+        humansText.text = $"{GameManager.Instance.FreeAssistant.Count}/{GameManager.Instance.AllCrew.Count}";
     }
 }
